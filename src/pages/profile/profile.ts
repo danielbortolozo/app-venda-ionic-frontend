78e39123663b3023533d5acadd92e8626899f802
Daniel R. Bortolozo <danielbortolozo@hotmail.com>
2020-05-21 20:52:49 -0300

    Usando cordova para tirar foto com a camera

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -4,13 +4,9 @@ import { StorageService } from '../../services/storage.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { API_CONFIG } from '../../config/api.config';
+//import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { CameraOptions, Camera } from '@ionic-native/camera';
 
-/**
- * Generated class for the ProfilePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -20,11 +16,15 @@ import { API_CONFIG } from '../../config/api.config';
 export class ProfilePage {
 
   cliente : ClienteDTO;
+  picture: string;
+  cameraOn: boolean = false;
+  photo: string = '';
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public storage: StorageService,
-              public clienteService : ClienteService) {
+              public clienteService : ClienteService,
+              public camera: Camera) {
   }
 
   ionViewDidLoad() {
@@ -52,7 +52,52 @@ export class ProfilePage {
                         this.cliente.imgUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;                        
                        },
                        error => {});
-            }
+  }
+  getCameraPicture() {
+    this.cameraOn = true;
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    console.log('chequei aqui na imagem >>>>>'); 
+    this.camera.getPicture(options).then((imageData) => {
+      this.picture = 'data:image/png;base64,' + imageData;
+      this.cameraOn = false;
+    }, (err) => {
+        console.log('Error camera....')
+    });
+  }
+
+  takePicture() {
+    this.photo = '';
+ 
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: true,
+      targetWidth: 100,
+      targetHeight: 100
+    }
+ 
+    this.camera.getPicture(options)
+      .then((imageData) => {
+        let base64image = 'data:image/jpeg;base64,' + imageData;
+        this.picture = base64image;
+ 
+      }, (error) => {
+        console.error(error);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }
+
+
+
 
 
 }
